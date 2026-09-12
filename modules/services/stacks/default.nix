a96b1a46214cd5ea3ed34496a30337be4944e208
Musa Murat <musamurad@proton.me>
2026-09-12 16:28:53 +0300

    fix: add systemd services to automatically start docker-compose stacks

--- modules/services/stacks/default.nix
+++ modules/services/stacks/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -7,4 +7,71 @@
     ./cloud.nix
     ./dev.nix
   ];
+
+  # --- DOCKER COMPOSE STACK SERVICES ---
+  # Automatically start docker-compose stacks via systemd
+
+  systemd.services.docker-compose-infra = {
+    description = "Docker Compose Infrastructure Stack";
+    after = [ "docker.service" ];
+    requires = [ "docker.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = "yes";
+      WorkingDirectory = "/srv/stacks/infra";
+      ExecStart = "${pkgs.docker-compose}/bin/docker-compose up -d";
+      ExecStop = "${pkgs.docker-compose}/bin/docker-compose down";
+      Restart = "on-failure";
+      RestartSec = "10s";
+    };
+  };
+
+  systemd.services.docker-compose-media = {
+    description = "Docker Compose Media Stack";
+    after = [ "docker.service" ];
+    requires = [ "docker.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = "yes";
+      WorkingDirectory = "/srv/stacks/media";
+      ExecStart = "${pkgs.docker-compose}/bin/docker-compose up -d";
+      ExecStop = "${pkgs.docker-compose}/bin/docker-compose down";
+      Restart = "on-failure";
+      RestartSec = "10s";
+    };
+  };
+
+  systemd.services.docker-compose-cloud = {
+    description = "Docker Compose Cloud Stack";
+    after = [ "docker.service" ];
+    requires = [ "docker.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = "yes";
+      WorkingDirectory = "/srv/stacks/cloud";
+      ExecStart = "${pkgs.docker-compose}/bin/docker-compose up -d";
+      ExecStop = "${pkgs.docker-compose}/bin/docker-compose down";
+      Restart = "on-failure";
+      RestartSec = "10s";
+    };
+  };
+
+  systemd.services.docker-compose-dev = {
+    description = "Docker Compose Development Stack";
+    after = [ "docker.service" ];
+    requires = [ "docker.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = "yes";
+      WorkingDirectory = "/srv/stacks/dev";
+      ExecStart = "${pkgs.docker-compose}/bin/docker-compose up -d";
+      ExecStop = "${pkgs.docker-compose}/bin/docker-compose down";
+      Restart = "on-failure";
+      RestartSec = "10s";
+    };
+  };
 }
